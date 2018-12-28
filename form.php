0d3427f0b633b004061b95a9c7a6373c85b8f6b0
NathanLeysGit <nathanley.personal@gmail.com>
2018-12-27 21:24:10 -0600

    Successful data request!

---
+++ form.php
@@ -0,0 +1,17 @@
+<?PHP ?>
+<!DOCTYPE html >
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="text/html">
+    <title>Document</title>
+  </head>
+
+  <body>
+    <p>
+    On the game, <?php echo $_POST["game"] ?>
+    you earned a score of <?php echo $_POST["score"]; ?>  
+    <p>
+  </body>
+</html>
