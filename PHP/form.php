b59add1e7b2a5a23d777dffb9f97f953de5f5e2d
NathanLeysGit <nathanley.personal@gmail.com>
2018-12-29 22:37:31 -0600

    Troubleshooting

--- PHP/form.php
+++ PHP/form.php
@@ -6,6 +6,8 @@
       if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $name = test_input($_POST["name"]);
         $score = test_input($_POST["score"]);
+        echo $name;
+        echo $score;
       }
 
       function test_input($data) {
@@ -14,116 +16,3 @@
         return $data;
       }
     ?>
-
-<!--Meta Data======================================================= -->
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Leader Board</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,700,800" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=EB+Garamond:400,700" rel="stylesheet">
-    <link href="../STYLE/leaderboardStyle.css" rel="stylesheet" type="text/css">
-  </head>
-
-
-<!--Header========================================================== -->    
-<body>
-  <div class="header"> 
-    <img class="logo" src= "../Lame Games Logo.png" >
-  </div>
-  
-
-
-
-<!--NavBar============================================================-->  
-  <nav>
-    <a href="./home.html">Home</a>
-    <a href="./download.html">Download</a>
-    <a href="./leaderboard.php">Leader Board</a>   
-  </nav> 
-  
-
-
-<!--Content========================================================= -->  
-    <a href="#bottom">
-      <button class="first">Submit Your Score</button>
-    </a>
-    <h2 class="special2">Game Title</h2>
-    <table class="special">
-      <thead>
-        <tr>
-          <th>Place</th>
-          <th>Name</th>
-          <th>Score</th>
-        </tr>
-      </thead>
-      
-      <tbody>
-        <tr>
-          <td>#1</td>
-          <td><?php echo $name?></td>
-          <td><?php echo $score?></td>
-        </tr>
-        <tr>
-          <td>#2</td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>#3</td>
-          <td></td>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
-    
-
-    <h2>Game Title</h2>
-    <table class="special">
-      <thead>
-        <tr>
-          <th>Place</th>
-          <th>Name</th>
-          <th>Score</th>
-        </tr>
-      </thead>
-      
-      <tbody>
-        <tr>
-          <td>#1</td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>#2</td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>#3</td>
-          <td></td>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
-<!--Submission Form===============================================-->
-    <h2 class="special2" id="bottom">Submit Your Score</h2>
-    <form class="special" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
-      <h4>Player Name</h4>
-      <input type="text" name="name" autocomplete="off" >
-    
-      <h4 class="formcomponent">Game</h4>
-      <select name="game">
-        <option>Game1</option>
-        <option>Game2</option>
-      </select>
-
-      <h4 class="formcomponent">Score</h4>
-      <input type="text" name="score" autocomplete = "off">
-      <br>
-      <button class="formcomponent important">Submit</button>
-    </form> 
-  </body>
-</html>
