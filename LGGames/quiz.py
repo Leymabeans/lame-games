3b6c31f4b26a22ca52d60be0bd8b08efca1e514f
NathanLeysGit <nathanley.personal@gmail.com>
2019-01-10 17:01:53 -0600

    testing python files

--- LGGames/quiz.py
+++ LGGames/quiz.py
@@ -1,5 +1,11 @@
 def instructions():
-    print("Welcome to my quiz!")
-    print("This is the end of the quiz, sorry")
+    print("Quizes in school can be very boring")
+    print("Making you think that all quizes are boring")
+    print("Not this one, I swear")
+    print("\n")
+    print("The point of this game is simple")
+    print("Answer as many correctly as you can")
+    print("But please, please don't get any wrong")
+    print("Trust me, you don't want to know what's going to happen")
 
 instructions()
