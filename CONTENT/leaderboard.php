f568d8a8a15d986815ef7de87e1f262cec056f7c
NathanLeysGit <nathanley.personal@gmail.com>
2019-03-23 21:38:33 -0500

    Created new pages for new content

---
+++ CONTENT/leaderboard.php
@@ -0,0 +1,131 @@
+<!--Meta Data======================================================= -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../MISC/LGLogo.ico" rel="shortcut icon">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link href="../STYLE/index.css" rel="stylesheet" type="text/css">
+    <link href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" rel="stylesheet" crossorigin="anonymous">
+  </head>
+
+
+
+<!--Nav Bar==========================================================-->
+  <body>
+    <nav>
+      <a class="home" href="./index.htm">Home</a>
+      <a class="news" href="./news.htm">News</a>
+      <a class="leaderboard" href="./leaderboard.php">Leaderboard</a>
+      <a class="profile" href="./profile.php">Profile</a>
+      <img src="../MISC/LGLogo.png">
+    </nav>
+
+<!--Game Leaderboard 1========================================================= --> 
+    <div class="leaderboard"> 
+      <a name="bottom"></a>
+      <h2 class="gametitle" >Cross Country Collin</h2><br>
+      <table>
+        <thead>
+          <tr>
+            <th class="place">Place</th>
+            <th class="name">Name</th>
+            <th class="score">Score</th>
+          </tr>
+        </thead>
+        
+        <tbody>
+          <tr>
+            <td>#1</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>#2</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>#3</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>#4</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>#5</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+
+
+
+
+
+  <!--Game Leaderboard 2========================================================= --> 
+      
+      <h2 class="gametitle">Escape</h2><br>
+      <table>
+        <thead>
+          <tr>
+            <th class="place">Place</th>
+            <th class="name">Name</th>
+            <th class="score">Score</th>
+          </tr>
+        </thead>
+        
+        <tbody>
+          <tr>
+            <td>#1</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>#2</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>#3</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>#4</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>#5</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table> 
+    </div>
+
+
+
+<!--Footer========================================================-->
+<footer>
+      <br><br><br>
+      <a href="https://github.com/nathanleysgit" target="_blank">
+        <i class="fab fa-github fa-3x"></i>
+      </a>
+
+      <a href="https://www.youtube.com" target="_blank">
+        <i class=" free fas fa-tv fa-3x" style="color: #ffffff"></i>
+      </a>
+
+      <br>
+      <span> 2018 Lame Games.</span>
+    </footer>  
+  </body>
+</html>
