46e3289423642d0b31e303054755f14af7a1ffad
NathanLeysGit <nathanley.personal@gmail.com>
2019-04-14 22:02:48 -0500

    Added automated success page after registration

---
+++ LoginSystem/registrationSuccess.php
@@ -0,0 +1,27 @@
+<!--Meta Data======================================================= -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Success</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../MISC/LGLogo.ico" rel="shortcut icon">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Chicle" rel="stylesheet">
+    <link href="./loginSystem.css" rel="stylesheet" type="text/css">
+    <link href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" rel="stylesheet" crossorigin="anonymous">
+    <script>
+      if(1 == 1) {
+        window.setTimeout(function(){
+        window.location.href = "../CONTENT/Profile.php";
+
+        }, 500);
+      }
+    </script>
+  </head>
+
+  <body class="none">
+    <h2 class="registration">Registration Successful!</h2>
+  </body>
+
+
