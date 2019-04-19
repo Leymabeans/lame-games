0ce3a08106ddec3e4c5a576d9bacfc06d83f3358
NathanLeysGit <nathanley.personal@gmail.com>
2019-04-19 10:12:10 -0500

    Added autorefresher page, bringing user to profile

---
+++ LoginSystem/loginSuccess.php
@@ -0,0 +1,24 @@
+<?php
+  session_start();
+  $y = $_SESSION['username'];
+  header('Refresh: 1; URL=../CONTENT/profile.php?'.$y);
+
+?>
+
+<!--Meta Data======================================================= -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Success</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../MISC/LGLogo.ico" rel="shortcut icon">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Chicle" rel="stylesheet">
+    <link href="./loginSystem.css" rel="stylesheet" type="text/css">
+    <link href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" rel="stylesheet" crossorigin="anonymous">
+  </head>
+
+  <body class="none">
+    <h2 class="registration">Login Successful!</h2>
+  </body>
