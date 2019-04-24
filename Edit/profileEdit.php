e0f279c5a150390a7462fb33f7c36c96f1f4057b
NathanLeysGit <nathanley.personal@gmail.com>
2019-04-24 18:19:41 -0500

    Positioned and designed profile edit page

--- Edit/profileEdit.php
+++ Edit/profileEdit.php
@@ -8,7 +8,7 @@
     <link href="../Miscellaneous/LGLogo.ico" rel="shortcut icon">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Merriweather:700" rel="stylesheet">
-    <link href="./index.css" rel="stylesheet" type="text/css">
+    <link href="./edit.css" rel="stylesheet" type="text/css">
     <link href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" rel="stylesheet" crossorigin="anonymous">
     <script>
       var myVar;
@@ -23,12 +23,13 @@
   </head>
   
   <body>
-    <div>
+    <div class="profileEdit">
+      <h2>Edit Profile</h2>
       <form class="bioAdd" action="./change.php" method="post"> 
-        Bio: <input type="text" name="bio"><br />
+        <input type="text" name="bio" placeholder="Add Bio"><br>
         <div class="addpic">
-            <input type="url" name="image">
-            Add Profile Image
+            <input type="url" name="image" placeholder="Add Profile URL">
+            
           </div>
         <input type="submit" value="Submit">
       </form>
