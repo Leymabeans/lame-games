38eef798ad4d6287f904d36844cad1e5e9d66d92
NathanLeysGit <nathanley.personal@gmail.com>
2019-08-01 10:31:22 -0500

    Grouped scripts together

--- private/scripts/edit-success.php
+++ private/scripts/edit-success.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  header('Refresh: 0.8; URL=../pages/profile.php?' . $_SESSION['username']);
+  header('Refresh: 1; URL=../Content/profile.php?'. $_SESSION['username']);
 
 ?>
 
@@ -8,16 +8,15 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Login Success</title>
+    <title>Edit Success</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="../images/LGLogo.ico" rel="shortcut icon">
+    <link href="../Miscellaneous/LGLogo.ico" rel="shortcut icon">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Chicle" rel="stylesheet">
-    <link href="../css/login-system.css" rel="stylesheet" type="text/css">
+    <link href="./edit.css" rel="stylesheet" type="text/css">
   </head>
 
-
   <body class="none">
-    <h2 class="registration">Login Successful!</h2>
+    <h2 class="successtext">Information Successfully Added!</h2>
   </body>
