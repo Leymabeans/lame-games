36d533849c17864e96aa6cc47f6529dbbaee881a
NathanLeysGit <nathanley.personal@gmail.com>
2019-08-03 15:18:00 -0500

    Styled profile page

--- private/js/index.js
+++ private/js/index.js
@@ -6,3 +6,24 @@ var myVar;
       function showPage() {
         document.getElementById("loader").style.display = "none";
       }
+
+      function display1() {
+        document.getElementById('global').innerHTML = "Game 1";
+        document.getElementById('game1').style.display = "block";
+        document.getElementById('game2').style.display = "none";
+        document.getElementById('game2').style.display = "none";
+      }
+      
+      function display2() {
+        document.getElementById('global').innerHTML = "Game 2";
+        document.getElementById('game1').style.display = "none";
+        document.getElementById('game2').style.display = "block";
+        document.getElementById('game3').style.display = "none";
+      }
+
+      function display3() {
+        document.getElementById('global').innerHTML = "Game 3";
+        document.getElementById('game1').style.display = "none";
+        document.getElementById('game2').style.display = "none";
+        document.getElementById('game3').style.display = "block";
+      }
