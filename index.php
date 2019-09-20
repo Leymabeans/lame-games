70a79fd9190f4e2de6f20d597f318d6ba4d1f322
NathanLeysGit <nathanley.personal@gmail.com>
2019-09-19 22:14:25 -0500

    Added scroll plugin linking

--- index.php
+++ index.php
@@ -12,6 +12,10 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     
+    <!--Scroll Plugin------------------------------>
+    <script src="./vendor/scroll-entrance/src/scroll-entrance.js"></script>
+    <style>[data-entrance] { visibility: hidden; }</style>
+
     <!--External files----------------------------->
     <link href="./images/favicon.ico" rel="shortcut icon">
     <link href="./stylesheets/index.css" rel="stylesheet" type="text/css">
@@ -29,7 +33,7 @@
         <span></span>
         <span></span>
         <ul id="menu">
-          <a class="page" href="./index.php">
+          <a class="page" href="">
             <li>Home</li>
           </a>
           <a class="page" href="./login/lor.php">
@@ -43,12 +47,53 @@
 
     <!--Header-------------------------------->
     <header class="container-fluid">
-      <div class="slideshow"></div>
-      <div class="text"> 
+      <div class="parrallax">
+        <div class="intro">
         <h1 class="display-6">Lame Games</h1>
         <p>Wasting time since '18</p>
-        <a class="btn btn-primary" href="#">Download Now</a>
+        </div>
       </div>
     </header>
+
+
+
+    <!--Information-------------------------->
+    <div class="container">
+      <div class="row info">
+        <section class="col-md-5">
+          <img class="img-fluid rounded" src="./images/development.jpg">
+        </section>
+        <section class="col-md-7">
+          <hgroup class="heading mb-2">
+            <h4 class="page-header text-muted">Develop</h4>
+            <h1 class="display-6 font-weight-bolder">Cutting Edge Games</h1>    
+          </hgroup>
+          <article>Here at Lame Games we use leading edge code to bring creative ideas to life. We take our development serious, as each game goes through rigorous development, testing, and deployment.</article>
+        </section>
+      </div>
+
+      <div class="row info">
+        <section class="col-md-7">
+          <hgroup class="heading mb-2">
+            <h4 class="page-header text-muted">Competition</h4>
+            <h1 class="display-6 font-weight-bolder">Messing Around</h1>    
+          </hgroup>
+          <article>Not all of our games are well developed. The creators at Lame Games hold friendly coding competitions where they develop the best game they can in an alloted period of time. These videos can be seen on our YouTube channel</article>
+        </section>
+        <section class="col-md-5">
+          <img class="img-fluid rounded" src="./images/competition.png">
+        </section>
+      </div>
+    </div>
+
+
+
+    <!--Download-------------------------->
+    <div class="row download">
+      <section class="col-md-4 offset-md-4">
+        <p>Want to play our games? Download our application!
+        <a class="btn btn-primary" href="">Download Now</a>
+      </section>
+    </div>
   </body>
 </html>
