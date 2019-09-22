9241811a123f26d1164101ce890f3d058480b012
NathanLeysGit <nathanley.personal@gmail.com>
2019-09-22 16:53:44 -0500

    Removed table functionality

--- js/index.js
+++ js/index.js
@@ -1,25 +1,9 @@
-/*Opening LOS modal===============================*/
-function los() {
+/*Opening search modal===============================*/
+function search() {
   var span = document.getElementsByClassName("close")[0];
-  var modal = document.getElementById("los");
+  var modal = document.getElementById("search");
   modal.style.display = "block";
   span.onclick = function() {
     modal.style.display = "none";
   }
-}
-
-
-/*Display game statistics========================*/
-function display1() {
-  document.getElementById('global').innerHTML = "Cross Country Collin";
-  document.getElementById('ccc').style.display = "block";
-  document.getElementById('dof').style.display = "none";
-  document.getElementById('game3').style.display = "none";
-}
-      
-function display2() {
-  document.getElementById('global').innerHTML = "Duel of the Fates";
-  document.getElementById('ccc').style.display = "none";
-  document.getElementById('dof').style.display = "block";
-  document.getElementById('game3').style.display = "none";
 }
